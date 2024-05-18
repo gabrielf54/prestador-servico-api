9694cb5cc62dcf47685cb524d90226a8a5ba48d3
gabriielf54 <gabriel.nuno22@gmail.com>
2024-05-18 18:51:44 -0300

    criação do projeto

---
+++ src/main/java/com/gabriel/prestadorservicosapi/PrestadorServicosApiApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.prestadorservicosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestadorServicosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestadorServicosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/prestadorservicosapi/PrestadorServicosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.prestadorservicosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestadorServicosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
